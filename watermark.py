e7c28be68a53405610ca4f528a7d205d590e0228
William Wu <w@qed.ai>
2016-02-09 13:11:18 -0800

    pep8 compliance

--- watermark.py
+++ watermark.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 # w@qed.ai
 
-import Image, ImageDraw, ImageFont, ImageEnhance
-import sys, os, getopt 
+import Image
+import ImageDraw
+import ImageFont
+import ImageEnhance
+import sys
+import os
+import getopt
 
-# usage
+
 def usage():
-    print('Usage:\n\t%s -i [input_filename] -o [output_filename]' % sys.argv[0])
+    print(
+        'Usage:\n\t%s -i [input_filename] -o [output_filename]' %
+        sys.argv[0])
     print('Optional:')
     print('\t%-30s %-30s' % ("-i [input_filename]", "input filename"))
     print('\t%-30s %-30s' % ("-o [output_filename]", "output filename"))
@@ -15,10 +22,11 @@ def usage():
     print('\t%-30s %-30s' % ("-p [position]", "position of watermark"))
     print('\t%-30s %-30s' % ("-c [opacity]", "opacity of textual watermark"))
     print('\t%-30s %-30s' % ("-t [text]", "text to be added as watermark"))
-    
+
 if len(sys.argv) < 2:
     usage()
     sys.exit()
+
 
 def reduce_opacity(im, opacity):
     """Returns an image with reduced opacity."""
@@ -32,7 +40,14 @@ def reduce_opacity(im, opacity):
     im.putalpha(alpha)
     return im
 
-def add_watermark_image(im, mark, position_x, position_y, mode=None, opacity=1):
+
+def add_watermark_image(
+        im,
+        mark,
+        position_x,
+        position_y,
+        mode=None,
+        opacity=1):
     """Adds a watermark to an image."""
     if opacity < 1:
         mark = reduce_opacity(mark, opacity)
@@ -40,7 +55,7 @@ def add_watermark_image(im, mark, position_x, position_y, mode=None, opacity=1):
         im = im.convert('RGBA')
     # create a transparent layer the size of the image and draw the
     # watermark in that layer.
-    layer = Image.new('RGBA', im.size, (0,0,0,0))
+    layer = Image.new('RGBA', im.size, (0, 0, 0, 0))
     if mode == 'tile':
         for y in range(0, im.size[1], mark.size[1]):
             for x in range(0, im.size[0], mark.size[0]):
@@ -58,28 +73,30 @@ def add_watermark_image(im, mark, position_x, position_y, mode=None, opacity=1):
     # composite the watermark with the layer
     return Image.composite(layer, im, layer)
 
-def add_watermark_text(im, text, angle=23, opacity=0.25, font='/Library/Fonts/Copperplate.ttc'):
+
+def add_watermark_text(im, text, angle=23, opacity=0.25,
+                       font='/Library/Fonts/Copperplate.ttc'):
     if im.mode != 'RGBA':
         im = im.convert('RGBA')
-    watermark = Image.new('RGBA', im.size, (0,0,0,0))
+    watermark = Image.new('RGBA', im.size, (0, 0, 0, 0))
     size = 2
     n_font = ImageFont.truetype(font, size)
     n_width, n_height = n_font.getsize(text)
     # grow size until the limit is reached
-    while n_width+n_height < watermark.size[0]:
+    while n_width + n_height < watermark.size[0]:
         size += 2
         n_font = ImageFont.truetype(font, size)
         n_width, n_height = n_font.getsize(text)
     draw = ImageDraw.Draw(watermark, 'RGBA')
     draw.text(((watermark.size[0] - n_width) / 2,
-              (watermark.size[1] - n_height) / 2),
-              text, (0,0,0), font=n_font)
-    watermark = watermark.rotate(angle,Image.BICUBIC)
+               (watermark.size[1] - n_height) / 2),
+              text, (0, 0, 0), font=n_font)
+    watermark = watermark.rotate(angle, Image.BICUBIC)
     alpha = watermark.split()[3]
     alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
     watermark.putalpha(alpha)
     return Image.composite(watermark, im, watermark)
- 
+
 
 # main method
 def main(argv):
@@ -100,7 +117,9 @@ def main(argv):
 
     # command-line arguments
     try:
-        opts, args = getopt.gnu_getopt(argv, "hi:o:f:w:p:c:t:a:s:x:y:m:", ["help", "input=","output=","format=","watermark_image","position=","opacity=","text=","angle=","show","position_x=","position_y=","mode="])
+        opts, args = getopt.gnu_getopt(
+            argv, "hi:o:f:w:p:c:t:a:s:x:y:m:", [
+                "help", "input=", "output=", "format=", "watermark_image", "position=", "opacity=", "text=", "angle=", "show", "position_x=", "position_y=", "mode="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -131,11 +150,11 @@ def main(argv):
             show_flag = True
         elif opt in ("-m", "--mode"):
             mode = arg
-    
+
     # argument checking
     if not os.path.exists(input_filename):
         sys.exit('ERROR: File %s was not found!' % input_filename)
-    if None in [input_filename,output_filename]:
+    if None in [input_filename, output_filename]:
         sys.exit('ERROR: Please provide both input and output filenames.')
     if None == watermark_filename and None == text:
         sys.exit('No watermark image or text provided. Exiting.')
@@ -155,20 +174,22 @@ def main(argv):
 
     # open input image
     im = Image.open(input_filename)
-    
+
     # image watermark
     if None != watermark_filename:
         mark = Image.open(watermark_filename)
         # watermark(im, mark, 'tile', 0.5).show()
         # watermark(im, mark, 'scale', 1.0).show()
         # watermark(im, mark, (100, 100), 0.5).show()
-        im_marked = add_watermark_image(im, mark, position_x, position_y, opacity)
+        im_marked = add_watermark_image(
+            im, mark, position_x, position_y, opacity)
         if show_flag:
             im_marked.show()
 
     # text watermark
     if None != text:
-        im_marked = add_watermark_text(im, text, angle, opacity, font='/Library/Fonts/Copperplate.ttc')
+        im_marked = add_watermark_text(
+            im, text, angle, opacity, font='/Library/Fonts/Copperplate.ttc')
 
     im_marked.save(output_filename, format)
 
